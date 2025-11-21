553b160a15c26afc45659eb95bd352b3a8fc3289
Dennis King Tran <dennis.tran@hotmail.de>
2025-11-21 19:44:14 +0100

    feat: Enhance legal notice page with detailed content and styling; add navigation functionality

--- src/app/imprint/legal-notice/legal-notice.component.ts
+++ src/app/imprint/legal-notice/legal-notice.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Inject, OnInit, OnDestroy } from '@angular/core';
+import { CommonModule, DOCUMENT } from '@angular/common';
+import { Router } from '@angular/router';
 import { HeaderComponent } from '../../shared/header/header.component';
 
 @Component({
@@ -9,6 +10,20 @@ import { HeaderComponent } from '../../shared/header/header.component';
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
-export class LegalNoticeComponent {
+export class LegalNoticeComponent implements OnInit, OnDestroy {
+  private bodyClass = 'imprint-bg-full';
 
+  constructor(@Inject(DOCUMENT) private document: Document, private router: Router) { }
+
+  ngOnInit(): void {
+    this.document.body.classList.add(this.bodyClass);
+  }
+
+  ngOnDestroy(): void {
+    this.document.body.classList.remove(this.bodyClass);
+  }
+
+  goHome(): void {
+    this.router.navigate(['/']);
+  }
 }
